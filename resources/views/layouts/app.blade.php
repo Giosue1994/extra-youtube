afb8fa6ccca4de96d60a61cb68443db52e1e7a34
Giosue1994 <giosuelentini1994@outlook.com>
2020-08-03 13:54:46 +0200

    start

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <title>@yield('title')</title>
+  </head>
+  <body>
+
+    <!-- HEADER -->
+    <header>
+
+    </header>
+
+    <!-- MAIN -->
+    <main>
+      @yield('main-content')
+    </main>
+
+    <!-- FOOTER -->
+    <footer>
+
+    </footer>
+
+    <script src="{{ asset('/js/app.js') }}"></script>
+
+  </body>
+</html>
---
+++ resources/views/youtube.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title')
+  Youtube
+@endsection
+
+@section('main-content')
+  <div class="wrapper">
+
+  </div>
+@endsection
